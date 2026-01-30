fa2628e9643f1ea91b10e13a634e7b5f3854aadb
Jahfar Ali <evjahfar@gmail.com>
2026-01-30 09:22:36 +0530

    icon

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth,  setPersistence,
-  browserLocalPersistence } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 import { getDatabase } from "firebase/database";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAM8oPKKJ-dIKANGOzjpRSkvqoQfZevAM0",
-  authDomain: "mk-fasion.firebaseapp.com",
-  databaseURL: "https://mk-fasion-default-rtdb.firebaseio.com",
-  projectId: "mk-fasion",
-  storageBucket: "mk-fasion.firebasestorage.app",
-  messagingSenderId: "9900303904",
-  appId: "1:9900303904:web:09083a183ca3930651be45",
-  measurementId: "G-H3P60Z7ER3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
