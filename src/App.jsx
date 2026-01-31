05b2758d24c4cf9fbe30817795695ff88a3f54fb
Jahfar Ali <evjahfar@gmail.com>
2026-01-31 23:10:53 +0530

    icon

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,56 @@
 
 
 
+// import { HashRouter, Routes, Route } from "react-router-dom";
+// import ShopLogin from "./pages/ShopLogin";
+// import ShopDashboard from "./pages/ShopDashboard";
+// import TodayAccount from "./pages/TodayAccount";
+
+// export default function App() {
+//   return (
+//     <HashRouter>
+//       <Routes>
+//         <Route path="/" element={<ShopLogin />} />
+//         <Route path="/dashboard" element={<ShopDashboard />} />
+//         <Route path="/today" element={<TodayAccount />} />
+//       </Routes>
+//     </HashRouter>
+//   );
+// }
+
+
+
 import { HashRouter, Routes, Route } from "react-router-dom";
 import ShopLogin from "./pages/ShopLogin";
 import ShopDashboard from "./pages/ShopDashboard";
 import TodayAccount from "./pages/TodayAccount";
+import ShopProtectedRoute from "./components/ShopProtectedRoute";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
+        {/* 🔓 PUBLIC */}
         <Route path="/" element={<ShopLogin />} />
-        <Route path="/dashboard" element={<ShopDashboard />} />
-        <Route path="/today" element={<TodayAccount />} />
+
+        {/* 🔐 PROTECTED */}
+        <Route
+          path="/dashboard"
+          element={
+            <ShopProtectedRoute>
+              <ShopDashboard />
+            </ShopProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/today"
+          element={
+            <ShopProtectedRoute>
+              <TodayAccount />
+            </ShopProtectedRoute>
+          }
+        />
       </Routes>
     </HashRouter>
   );
